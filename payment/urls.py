1261fbe4c0441c1f6e571c5f678f21b1e8f3113f
techweb dev <amitkadivar.ce@gmail.com>
2021-10-20 00:56:40 +0530

    clear code

--- payment/urls.py
+++ payment/urls.py
@@ -6,14 +6,6 @@ app_name='payment'
 urlpatterns = [
     path('process/',views.create_order,name="process"),
     path('response/',views.payment_process,name="response"),
-    # path('response/',views.response,name="response"),
     path('canceled/',views.payment_canceled,name="canceled"),
     re_path(r'^paypal/', include('paypal.standard.ipn.urls'),name="paypal-ipn"),
-    # re_path(r'^payment_process/$', api_views.payment_process, name='payment_process' ),
-
-    # re_path(r'^payment_done/$', TemplateView.as_view(template_name= "pets/payment_done.html"), name='payment_done'),
-
-    # re_path(r'^payment_canceled/$', TemplateView.as_view(template_name= "pets/payment_canceled.html"), name='payment_canceled'),
-    
-
 ]
